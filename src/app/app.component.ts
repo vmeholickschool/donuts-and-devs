318e978bb02844216432eddb50e50156bef503f4
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-05-27 10:43:20 -0400

    trying to fix stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule,RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
+import { DonutsComponent } from './components/donuts/donuts.component';
+import { FamousPeopleComponent } from './components/famous-people/famous-people.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, RouterLink, RouterOutlet, RouterLinkActive, DonutsComponent, FamousPeopleComponent],
 })
 export class AppComponent {
   title = 'Donuts and Devs';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,17 @@ import { AppComponent } from './app.component';
 import { DonutsComponent } from './components/donuts/donuts.component';
 import { FamousPeopleComponent } from './components/famous-people/famous-people.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   {
     path: '',
     component: AppComponent,
     children: [
-      { path: '', redirectTo: 'donuts', pathMatch: 'full' },
+      { path: '', component: DonutsComponent},
       { path: 'donuts', component: DonutsComponent },
       { path: 'famous-people', component: FamousPeopleComponent }
     ]
   }
+  
 ];
 
 export default routes;
