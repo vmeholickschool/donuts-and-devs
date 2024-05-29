4f1e15c184787b0fec0eddc29a5a130d9ccfd2f5
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-05-29 10:53:35 -0400

    xx

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,7 @@ import { FamousPeopleComponent } from './components/famous-people/famous-people.
 export const routes: Routes = [
   {
     path: '',
-    component: AppComponent,
     children: [
-      { path: '', component: DonutsComponent},
       { path: 'donuts', component: DonutsComponent },
       { path: 'famous-people', component: FamousPeopleComponent }
     ]
